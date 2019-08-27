96686c2e73d1e6ad08bd450e6f663a5110ea58f1
registriren <emailresurs@gmail.com>
2019-08-27 21:58:54 +1000

    comment service bitly

--- filelink.py
+++ filelink.py
@@ -3,7 +3,7 @@
 
 
 from botapitamtam import BotHandler
-from bitlyshortener import Shortener
+#from bitlyshortener import Shortener  #для использования требуется python3.7
 import json
 import requests
 
@@ -11,9 +11,9 @@ config = 'config.json'
 with open(config, 'r', encoding='utf-8') as c:
     conf = json.load(c)
     token = conf['access_token']
-    token_bitly = [conf['bitly_token']]
+    #token_bitly = [conf['bitly_token']]
 
-shortener = Shortener(tokens=token_bitly, max_cache_size=8192)
+#shortener = Shortener(tokens=token_bitly, max_cache_size=8192)
 bot = BotHandler(token)
 
 def main():
@@ -37,7 +37,7 @@ def main():
                         "payload": 'long'
                         }]
             bot.send_buttons("Тип ссылки", buttons, chat_id)
-            url_short = [url]
+            #url_short = [url]
             url_long = url
         #else:
         #    bot.send_message('Нет ссылки', chat_id)
@@ -46,9 +46,9 @@ def main():
                     params = {'url': url_long}
                     res_clck = requests.get('https://clck.ru/--', params)
                     link_clck = res_clck.text
-                    link_bitly = shortener.shorten_urls(url_short)[0]
+                    #link_bitly = shortener.shorten_urls(url_short)[0]
                     #print(link_bitly)
-                    bot.send_message(link_bitly, chat_id)
+                    #bot.send_message(link_bitly, chat_id)
                     bot.send_message(link_clck, chat_id)
         elif payload == 'long':
                     bot.send_message(str(url_long), chat_id)
