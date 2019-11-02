77750950f408e3a6e52385a2bdc7b252487cbe3e
registriren <emailresurs@gmail.com>
2019-11-02 10:07:22 +1000

    download youtube upd

--- filelink.py
+++ filelink.py
@@ -37,7 +37,6 @@ def main():
     while True:
         last_update = bot.get_updates(marker)
         # тут можно вставить любые действия которые должны выполняться во время ожидания события
-        print('ждем')
         if last_update == None:  # проверка на пустое событие, если пусто - возврат к началу цикла
             continue
         marker = bot.get_marker(last_update)
